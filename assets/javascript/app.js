f84c6a54f94dd98007738937f07371b96d2c26e0
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-03-27 18:38:26 -0700

    Added in start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+    //Write a start button to the document inside the "start button" div
+    //In theory, we could have the button included in the base html and clear it
+    //when the user presses start. However, it is cleaner to have everything that changes
+    //in one place.
 
+    var startButton = "<button id='startButton'>Start Game!</button>";
+    $("#startButtonContainer").append(startButton);
+
+    //Clicking the button should initiate ...
+    $("#startButton").click(function(){
+        
+    });
 
 });
