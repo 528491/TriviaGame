7f02f0189aef5b9f9ac3642020d72f48557ef17c
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-03-28 00:35:39 -0700

    Successfully added timer functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
     
+    //Creating a class to act as the game timer
+    var intervalId;
+    var clockRunning = false;
+
+    var gameTimer = {
+        timeRemaining: 30,
+
+        start: function(){
+            if (!clockRunning){
+                intervalId = setInterval(gameTimer.count, 1000);
+                clockRunning = true;
+            }
+        },
+
+        count: function(){
+           gameTimer.timeRemaining--;
+           console.log(gameTimer.timeRemaining);
+
+           //Update the page to reflect the time remaining
+           var timeDisplay = $("<p>");
+           timeDisplay.text("Time Remaining: " + gameTimer.timeRemaining)
+           $("#timerContainer").empty();
+           $("#timerContainer").append(timeDisplay);
+            
+           //Call function "endGame() if time <= 0"
+           if (gameTimer.timeRemaining <= 0){
+                gameTimer.stop();
+           }
+        },
+
+        //Called when we reach time 0. 
+        stop: function(){
+            clearInterval(intervalId);
+            clockRunning = false;
+        },
+
+        //We will need this function when a new game begins
+        reset: function(){
+            clearInterval(intervalId);
+            clockRunning = false;
+            gameTimer.timeRemaining=30;
+        }
+
+    };
+
     //Write a start button to the document inside the "start button" div
     //In theory, we could have the button included in the base html and clear it
     //when the user presses start. However, it is cleaner to have everything that changes
@@ -10,15 +55,23 @@ $(document).ready(function(){
         startButton.text("Start Game!");
         
         $("#startButtonContainer").append(startButton);
+
+        gameTimer.reset();
     }
 
     //We will call this function at least once, when the page initially loads. 
     newGame();
     
+    //function endGame()
+    //Display Game Over Message!
+    //Display Score
+    //Display button offering for a new game
+    //button on Click calls newGame()
+    
 
     //Clicking the button should initiate ...
     $("#startButton").click(function(){
-        
+        gameTimer.start();
     });
 
 });
