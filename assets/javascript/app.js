eccf2f328436809b9cd86b861751a588495a7801
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-03-27 23:36:26 -0700

    Refactored created the start button into a function called newGame()

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+    
     //Write a start button to the document inside the "start button" div
     //In theory, we could have the button included in the base html and clear it
     //when the user presses start. However, it is cleaner to have everything that changes
     //in one place.
+    function newGame(){
+        var startButton = $("<button>");
+        startButton.attr("id", "startButton");
+        startButton.text("Start Game!");
+        
+        $("#startButtonContainer").append(startButton);
+    }
 
-    //var startButton = "<button id='startButton'>Start Game!</button>";
-    var startButton = $("<button>");
-    startButton.attr("id", "startButton");
-    startButton.text("Start Game!");
+    //We will call this function at least once, when the page initially loads. 
+    newGame();
     
-    $("#startButtonContainer").append(startButton);
 
     //Clicking the button should initiate ...
     $("#startButton").click(function(){
-        alert(soMuchStuff)
+        
     });
 
 });
