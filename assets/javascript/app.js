8524027239a3826306c730b6fe00dbda327f7b67
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-03-28 23:48:59 -0700

    Added buttons and questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     
+    //event listener defining the functionality of each button when clicked
+    $(".quizOptions").click(function(){
+        console.log("Clicked!");
+        $(this).parent().attr("selectedAnswer", this.text());
+        console.log(this.text());
+    });
+
+
     //Creating a class to act as the game timer
     var intervalId;
     var clockRunning = false;
@@ -34,6 +42,7 @@ $(document).ready(function(){
         stop: function(){
             clearInterval(intervalId);
             clockRunning = false;
+            endGame();
         },
 
         //We will need this function when a new game begins
@@ -61,17 +70,110 @@ $(document).ready(function(){
 
     //We will call this function at least once, when the page initially loads. 
     newGame();
+
+    //This function will randomly select a number of questions from the master "questions" array
+    //contained within questions.js
+    function selectQuestions(){
+        return questions;
+    }
+
+    
+
+    //this question will display the questions - called whenever a new game is started
+    function displayQuestions(){
+        questions = selectQuestions();
+        console.log(questions);
+
+        //correct answers array
+
+        for (question in questions){
+            var questionDiv = $("<div>");
+            questionDiv.attr("id", "questionNumber"+question);
+            
+            //Display the text of the first question
+            var questionText = $("<p>");
+            questionText.text(questions[question].questionText);
+            questionDiv.append(questionText);
+
+            questionDiv.attr("correctAnswer", questions[question].correctAnswer);
+
+            questionDiv.attr("selectedAnswer", "");
+            var selectedAnswerDisplay = $("<p>");
+            selectedAnswerDisplay.text("Selected Answer: " + questionDiv.attr("selectedAnswer"));
+            questionDiv.append(selectedAnswerDisplay);
+            
+            //Create a button for option 1
+            //all should share a class so on click can apply to all of them
+            //object itself will 
+            var buttonOption1 = $("<button>");
+            buttonOption1.attr("class","quizOptions");
+            buttonOption1.text(questions[question].option1);
+            questionDiv.append(buttonOption1);
+
+            
+            //Create a button for option 2
+            var buttonOption2 = $("<button>");
+            buttonOption2.attr("class","quizOptions");
+            buttonOption2.text(questions[question].option2);
+            questionDiv.append(buttonOption2);
+
+            //Create a button for option 3
+            var buttonOption3 = $("<button>");
+            buttonOption3.attr("class","quizOptions");
+            buttonOption3.text(questions[question].option3);
+            questionDiv.append(buttonOption3);
+            
+            //Create a button for option 4
+            var buttonOption4 = $("<button>");
+            $(buttonOption4).addClass("quizOptions");
+            buttonOption4.text(questions[question].option4);
+            questionDiv.append(buttonOption4);
+            
+            
+            
+            $("#questionsContainer").append(questionDiv);
+        }
+
+    }
+
+    
+
+    //Function startGame()
+    //Starts the timer in the countdown timer object, changes the UI elements to display 
+    //questions on the screen
+    function startGame(){
+        displayQuestions();
+        $("#startButtonContainer").empty();
+        gameTimer.start();
+    }
+    
+    
     
     //function endGame()
     //Display Game Over Message!
     //Display Score
     //Display button offering for a new game
     //button on Click calls newGame()
+    function endGame(){
+        $("#timerContainer").empty();
+        var allDoneMessage = $("<h2>");
+        allDoneMessage.text("All Done!");
+        $("#gameDisplay").append(allDoneMessage);
+
+        var newGameButton = $("<button>");
+        newGameButton.text("New Game");
+        newGameButton.attr("id", "newGameButton");
+        $("#gameDisplay").append(newGameButton);
+    }
     
 
-    //Clicking the button should initiate ...
+    //Clicking the button should call the startGame function
     $("#startButton").click(function(){
-        gameTimer.start();
+        startGame();
     });
 
+    //Clicking the New Game button should start the process again
+    $("#newGameButton").click(function(){
+        newGame();
+    });
 });
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,53 @@
 //This is a separate javascript file designed to hold all the questions
+
+var questions = [
+    {
+        questionText: "Who was Captain of Deep Space 9 During the Dominion War?",
+        option1: "Jean-Luc Picard",
+        option2: "Benjamin Sisko",
+        option3: "Kathryn Janeway",
+        option4: "Alina Nechayev",
+        correctAnswer: "Benjamin Sisko"
+    },
+    {
+        questionText: "Which Starfleet Captain Survived Assimilation by the Borg?",
+        option1: "Christopher Pike",
+        option2: "Neelix",
+        option3: "Jonathan Archer",
+        option4: "Jean-Luc Picard",
+        correctAnswer: "Jean-Luc Picard"
+    },
+    {
+        questionText: "What Class was the USS Enterprise D?",
+        option1: "Sovereign Class",
+        option2: "Enterprise Class",
+        option3: "Galaxy Class",
+        option4: "Voyage Class",
+        correctAnswer: "Galaxy Class"
+    },
+    {
+        questionText: "Who was Captain of Deep Space 9 During the Dominion War?",
+        option1: "Jean-Luc Picard",
+        option2: "Benjamin Sisko",
+        option3: "Kathryn Janeway",
+        option4: "Alina Nechayev",
+        correctAnswer: "Benjamin Sisko"
+    },
+    {
+        questionText: "Who was Captain of Deep Space 9 During the Dominion War?",
+        option1: "Jean-Luc Picard",
+        option2: "Benjamin Sisko",
+        option3: "Kathryn Janeway",
+        option4: "Alina Nechayev",
+        correctAnswer: "Benjamin Sisko"
+    },
+    {
+        questionText: "Who was Captain of Deep Space 9 During the Dominion War?",
+        option1: "Jean-Luc Picard",
+        option2: "Benjamin Sisko",
+        option3: "Kathryn Janeway",
+        option4: "Alina Nechayev",
+        correctAnswer: "Benjamin Sisko"
+    }
+
+];
